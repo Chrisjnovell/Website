47e64fa974b4dfc957d537a411a305391878a495
unknown <chrisjnovell@gmail.com>
2020-04-01 17:11:42 +0100

    Created server JS file with basic js for server

---
+++ server.js
@@ -0,0 +1,14 @@
+'use strict'
+//Constant variables
+const express = require("express")
+const app = express()
+const port = 8080;
+const fs = require("fs")
+
+//app.use stuff
+app.use(express.static("images"))
+app.use(express.static("client"))
+app.use(express.urlencoded())
+app.use(express.json())
+
+app.listen(port, () => console.log("server is listening"))
